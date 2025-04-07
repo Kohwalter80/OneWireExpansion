061108af572c530b1abc6514b677a30a0a051da5
Kohwalter80 <kohwalter@gmail.com>
2025-04-07 19:57:12 -0300

    Add files via upload

--- OneWireExpansion/examples/enviarBasico/enviarBasico.ino
+++ OneWireExpansion/examples/enviarBasico/enviarBasico.ino
@@ -1,9 +1,11 @@
 #include "OneWireExpansion.h"
 
-OneWireExpansion saida(3); // envia os dados pelo pino D3
+OneWireExpansion saida(3);  // envia os dados pelo pino D3
 
 void setup() {}
 
 void loop() {
-  for (byte i = 0; i < 128; i++) saida.enviaByte(i);
+  byte i;
+  for (i = 0; i < 128; i++) saida.enviarByte(i);
+  for (i = 0; i < 128; i++) saida.enviarByte(i << 1, false);
 }
